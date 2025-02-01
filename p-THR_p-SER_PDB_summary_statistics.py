7b130d2c72bc49df95bea443761ab045273a2394
Jack Shaw <112831590+JackShaw1@users.noreply.github.com>
2025-02-01 16:01:51 -0500

    Update p-THR_p-SER_PDB_summary_statistics.py

--- p-THR_p-SER_PDB_summary_statistics.py
+++ p-THR_p-SER_PDB_summary_statistics.py
@@ -1,6 +1,7 @@
 from Bio.PDB import PDBParser, NeighborSearch
 import os
 from collections import Counter
+import csv
 
 amino_acids = [
     "ALA",  # Alanine
@@ -22,7 +23,10 @@ amino_acids = [
     "THR",  # Threonine
     "TRP",  # Tryptophan
     "TYR",  # Tyrosine
-    "VAL"   # Valine
+    "VAL",  # Valine
+    "SEP",
+    "TPO",
+    "PTR"
 ]
 
 def analyze_struct(structure, ns):
@@ -45,6 +49,21 @@ def analyze_struct(structure, ns):
                                 if f"{atom2.get_parent().get_parent().get_id()} - {atom2.get_parent().get_id()[1]}" not in taken and atom2.get_parent().get_parent().get_id() != chain.get_id() and atom2.get_name() not in ['N', 'CA', 'C', 'O'] and atom2.get_parent().get_resname() in amino_acids:
                                     taken.append(f"{atom2.get_parent().get_parent().get_id()} - {atom2.get_parent().get_id()[1]}")
                                     contacts_holder.append(atom2.get_parent().get_resname())
+                            chain_neighbor_counter = []
+                            inter_index = None
+                            inter_distance = 100
+                            for atom2 in neighbors:
+                                if atom2.get_parent().get_parent().get_id() != chain.get_id() and atom2.get_parent().get_resname() in amino_acids and atom2.get_parent().get_parent().get_id() not in chain_neighbor_counter:
+                                    chain_neighbor_counter.append(atom2.get_parent().get_parent().get_id())
+                            for atom2 in neighbors:
+                                if atom2.get_parent().get_parent().get_id() != chain.get_id() and atom2.get_parent().get_resname() in amino_acids:
+                                    if atom2 - atom < inter_distance:
+                                        inter_distance = atom2 - atom
+                                        inter_index = atom2.get_parent().get_id()[1]
+                            if len(chain_neighbor_counter) == 1:
+                                with open('output_sep_tpo_jan_29.csv', 'a', newline='') as outfile:
+                                    writer = csv.writer(outfile)
+                                    writer.writerow([structure.get_id(), chain.get_id(), residue.get_id()[1], chain_neighbor_counter[0], inter_index])
                             if inter_id == True:
                                 total_num_inter_holder += 1
         break
@@ -57,8 +76,11 @@ if __name__ == "__main__":
     contacts = []
     parser = PDBParser(QUIET = True)
     print(f'\nTotal number of files to analyze: {len(os.listdir('pdb_structures_SEP_TPO'))}\n')
+    file_counter = 0
     for file in os.listdir('pdb_structures_SEP_TPO'):
-        structure = parser.get_structure('struct', f"pdb_structures_sep_tpo/{file}")
+        print(file_counter)
+        file_counter += 1
+        structure = parser.get_structure(file.split('.')[0], f"pdb_structures_sep_tpo/{file}")
         ns = NeighborSearch(list(structure.get_atoms()))
         results = analyze_struct(structure, ns)
         total_num_phos += results[0]
